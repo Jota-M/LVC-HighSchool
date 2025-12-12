9e8ac62f74e25c8a8edc5344966bad83e8a0aed0
JoA <cooper17john@gmail.com>
2025-12-12 12:07:41 -0400

    Fix: Corregir configuración de Next.js

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,8 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+const nextConfig = {
   eslint: {
-    // Ignora errores de ESLint durante el build
     ignoreDuringBuilds: true,
   },
   typescript: {
-    // Ignora errores de TypeScript durante el build
     ignoreBuildErrors: true,
   },
 };
